0ab8cd87194e0f875b130038ad2b86d4b4a4c751
Jay Hariani <jay@govtribe.com>
2014-02-13 18:58:09 -0500

    Updated facet style.

--- app/views/facets.blade.php
+++ app/views/facets.blade.php
@@ -1,20 +1,24 @@
 <div class="list-group" id="facets-list">
 @if (isset($facets))
 	@foreach ($facets as $type => $data)
-		@if (empty($data))
-			<li class="list-group-item active">
-				<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">No {{ Str::title($type) }}</blockquote>
-			</li>
-		@else
-			<li href="#" class="list-group-item active">
-				<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">{{ Str::title($type) }}</blockquote>
-			</li>
-			@foreach ($data as $name => $count)
-				<a href="#" class="list-group-item" id={{str_replace(' ', '-', $type.'xxx'.$name)}}>
-				<span class="badge">{{$count}}</span>
-				<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">{{$name}}</blockquote>
-				</a>
-			@endforeach
+		@if (!empty($data))
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">{{ Str::title($type) }}</h3>
+				</div>
+				<ul class="list-group">
+				@foreach ($data as $name => $count)
+					<li class="list-group-item">
+						<a href="#" id={{ str_replace(' ', '-', $type.'xxx'.$name) }}>
+							<span class="badge">
+								{{$count}}
+							</span>
+							{{ $name }} 
+						</a>
+					</li>
+				@endforeach
+				</ul>
+			</div>				
 		@endif
 	@endforeach
 @endif
