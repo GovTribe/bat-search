86c671f69f0bd0956d7383fe8170f8b05fb32e04
Jay Hariani <jay@govtribe.com>
2014-02-13 18:58:36 -0500

    Updated footer style.

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,16 +1,12 @@
-<footer>
-	<div class="row">
-		<div class="col-md-12">
-			<table style="width: 100%;">
-				<tbody>
-					<tr>
-						<td style="text-align: center; vertical-align: middle;">
-							<img src="assets/gtlogo.png" width="10%">
-							<h6>Data &copy; {{ date("Y") }} GovTribe, Inc.</h6>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+<div id="footer">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div style="text-align: center; vertical-align: middle; padding: 5px;">
+					<span class="text-muted" style="font-size: .9em;">Data &copy; {{ date("Y") }}</span>
+					<a href="http://www.govtribe.com">{{ HTML::image("assets/gtlogo.png", "GovTribe") }}</a>
+				</div>
+			</div>
 		</div>
 	</div>
-</footer>
+</div>
