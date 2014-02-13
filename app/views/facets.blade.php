4112d3062f7ca373db99e2efcd5da293684f3da4
Nate Nash <nate@govtribe.com>
2014-02-13 11:38:55 -0500

    Update to facets.blade.php allowing for default state of no facet

--- app/views/facets.blade.php
+++ app/views/facets.blade.php
@@ -2,46 +2,46 @@
 	<li class="list-group-item">
 		<b>Agencies</b>
 	</li>
+    @if (!isset($facets))
+        <li class="list-group-item">
+        <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">No Agencies</blockquote>
+        </li>
+    @else
+        @foreach ($facets["Agencies"] as $agencyFacet)
+            <li class="list-group-item">
+            <span class="badge">$agencyFacet["value"]</span>
+            <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">{{$agencyFacet["name"]}}</blockquote>
+            </li>
+        @endforeach
+    @endif
 	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Agency for International Development</blockquote>
+		<b>Offices</b>
 	</li>
-	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Department of the Army</blockquote>
-	</li>
-	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Department of the Air Force</blockquote>
-	</li>
-	<li class="list-group-item">
-		<b><u>Offices</u></b>
-	</li>
-	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Office of Acquisition</blockquote>
-	</li>
-	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Fort Hood</blockquote>
-	</li>
-	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Bureau of Prisons</blockquote>
-	</li>
+    @if (!isset($facets))
+        <li class="list-group-item">
+        <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">No Offices</blockquote>
+        </li>
+    @else
+        @foreach ($facets["Offices"] as $officeFacet)
+            <li class="list-group-item">
+            <span class="badge">$officeFacet["value"]</span>
+            <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">{{$officeFacet["name"]}}</blockquote>
+            </li>
+        @endforeach
+    @endif
 	<li class="list-group-item">
 		<b>Categories</b>
 	</li>
-	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Wholesale Food Production</blockquote>
-	</li>
-	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Agriculture: Crop Production</blockquote>
-	</li>
-	<li class="list-group-item">
-		<span class="badge">42</span>
-		<blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">Spcial Food Purchases</blockquote>
-	</li>
+    @if (!isset($facets))
+        <li class="list-group-item">
+        <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">No Categories</blockquote>
+        </li>
+    @else
+        @foreach ($facets["Categories"] as $agencyFacet)
+            <li class="list-group-item">
+            <span class="badge">$categoryFacet["value"]</span>
+            <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">{{$categoryFacet["name"]}}</blockquote>
+            </li>
+        @endforeach
+    @endif
 </ul>
