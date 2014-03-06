3acb6925af3a8be553d3f0c008dade17a39dd4c9
Jay Hariani <jay@govtribe.com>
2014-03-06 10:03:06 -0500

    Fixes #29

--- app/views/searchform.blade.php
+++ app/views/searchform.blade.php
@@ -1,4 +1,4 @@
-{{ Form::open(array('action' => 'SearchController@query', 'id' => "big-search")) }}
+{{ Form::open(array('action' => 'SearchController@query', 'id' => 'big-search')) }}
 	<input name="facet" type="hidden" value="">
-	<input name="query" type="text" id="big-search" class="form-control input-lg" placeholder="Start typing to search" style="border:none; height:100px; font-size:2em; outline:none; box-shadow:none !important">
+	<input name="query" type="text" id="big-search" class="form-control input-lg" placeholder="Start typing to search" autofocus="autofocus" style="border:none; height:100px; font-size:2em; outline:none; box-shadow:none !important">
 {{ Form::close() }}
