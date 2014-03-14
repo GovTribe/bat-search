3612cfa998595fd598ee7f26e31f8d7c3580edf2
Jay Hariani <jay@govtribe.com>
2014-03-14 13:31:49 +0530

    Test coverage WIP.

--- app/tests/acceptance/PerformSearchCept.php
+++ app/tests/acceptance/PerformSearchCept.php
@@ -3,7 +3,6 @@
 $I = new WebGuy($scenario);
 $I->amOnPage('/');
 $I->wantTo('perform a search');
-$I->fillField('query','UAV');
-$I->click('Submit');
+$I->submitForm('#big-search', array('query' => 'UAV'));
 $I->waitForJS("return $.active == 0;", 60);
 $I->see('Department of the Air Force');
