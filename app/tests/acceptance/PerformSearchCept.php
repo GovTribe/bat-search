5bff0694e63db59ee3f70b68da8872b2242ccc17
Jay Hariani <jay@govtribe.com>
2014-03-14 13:36:48 +0530

    Test coverage WIP.

--- app/tests/acceptance/PerformSearchCept.php
+++ app/tests/acceptance/PerformSearchCept.php
@@ -6,3 +6,7 @@ $I->wantTo('perform a search');
 $I->submitForm('#big-search', array('query' => 'UAV'));
 $I->waitForJS("return $.active == 0;", 60);
 $I->see('Department of the Air Force');
+$I->see('Uav');
+$I->see('Modification/Amendment');
+$I->seeLink('Details');
+$I->see('DESCRIPTIONThis is NOT a pre-solicitation notice pursuant to FAR Part 5, but a Sources Sought market survey to identify sources that are capable of providing specialized equipment and engineering services to the 96 TW for Small Unmanned Air vehicle Systems (SUAS) services to support Test Missions. The primary Missions will be conducted over both the Land and Water Ranges of the Eglin Complex. The Missions will be conducted in Restricted Air Space R2915A, R2915B, R2914A, and R2919A and Warning Ar');
