5ee2d61c28f216abc9b75be5ab4089b74874fda8
Jay Hariani <jay@govtribe.com>
2014-03-14 14:16:50 +0530

    Added test coverage.

--- app/tests/acceptance/PerformSearchCept.php
+++ app/tests/acceptance/PerformSearchCept.php
@@ -5,8 +5,38 @@ $I->amOnPage('/');
 $I->wantTo('perform a search');
 $I->submitForm('#big-search', array('query' => 'UAV'));
 $I->waitForJS("return $.active == 0;", 60);
+
+// Results are displayed.
 $I->see('Department of the Air Force');
 $I->see('Uav');
 $I->see('Modification/Amendment');
-$I->seeLink('Details');
 $I->see('DESCRIPTIONThis is NOT a pre-solicitation notice pursuant to FAR Part 5, but a Sources Sought market survey to identify sources that are capable of providing specialized equipment and engineering services to the 96 TW for Small Unmanned Air vehicle Systems (SUAS) services to support Test Missions. The primary Missions will be conducted over both the Land and Water Ranges of the Eglin Complex. The Missions will be conducted in Restricted Air Space R2915A, R2915B, R2914A, and R2919A and Warning Ar');
+
+//Facets are displayed.
+$I->seeElement('#facets-list');
+
+//Project details.
+$I->seeLink('Details');
+$I->click('Details');
+$I->waitForJS("return $.active == 0;", 60);
+$I->see('Last Updated');
+$I->see('2013-Dec-05');
+$I->see('Source');
+$I->seeLink('FedBizOps.gov');
+$I->see('Goods or Services');
+$I->see('Goods');
+$I->click('Close');
+$I->waitForJS("return $.active == 0;", 60);
+
+//POC
+$I->seeLink('Monica Payton');
+$I->click('Monica Payton');
+$I->waitForJS("return $.active == 0;", 60);
+$I->see('Email');
+$I->see('monica.payton@us.mil');
+$I->see('Agency');
+$I->see('Department of the Air Force');
+$I->see('Office');
+$I->see('Air Force Materiel Command');
+$I->click('Close');
+$I->waitForJS("return $.active == 0;", 60);
